3d6c97861c9d6b6dcc7c1491f2b53faf55590f63
Derek Donkor <67352482+thexenon@users.noreply.github.com>
2025-06-19 06:46:44 +0000

    Update productController.js

--- backend/controllers/productController.js
+++ backend/controllers/productController.js
@@ -8,8 +8,12 @@ exports.setRequiredIds = (req, res, next) => {
   next();
 };
 
-exports.getAllProducts = factory.getAll(Product);
-exports.getProduct = factory.getOne(Product);
+exports.getAllProducts = factory.getAll(Product, {
+  path: 'category store reviews',
+});
+exports.getProduct = factory.getOne(Product, {
+  path: 'category store reviews',
+});
 exports.createProduct = factory.createOne(Product);
 exports.updateProduct = factory.updateOne(Product);
 exports.deleteProduct = factory.deleteOne(Product);
